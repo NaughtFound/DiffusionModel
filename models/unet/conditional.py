09e427b524a074552d7bc37546e5c72e72e758db
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-01-29 11:37:50 +0330

    ConditionalUNet updated

--- models/unet/conditional.py
+++ models/unet/conditional.py
@@ -9,15 +9,30 @@ class ConditionalUNet(UNet):
         self,
         num_classes: int,
         in_channels: int = 3,
+        mid_channels: int = 64,
         out_channels: int = 3,
         time_dim: int = 256,
+        emb_dim: int = 256,
+        features: list[int] = [128, 256],
+        neck_features: list[int] = [512],
     ) -> None:
-        super().__init__(in_channels, out_channels, time_dim)
+        super().__init__(
+            in_channels,
+            mid_channels,
+            out_channels,
+            time_dim,
+            emb_dim,
+            features,
+            neck_features,
+        )
 
         self.label_emb = nn.Embedding(num_classes, time_dim)
 
     def forward(
-        self, x: torch.Tensor, t: torch.Tensor, y: torch.Tensor = None
+        self,
+        x: torch.Tensor,
+        t: torch.Tensor,
+        y: torch.Tensor = None,
     ) -> torch.Tensor:
         t = self.time_encoding(t)
 
