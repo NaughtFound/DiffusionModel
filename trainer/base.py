f844a91122ea7290b3e58c070c6532bdd9222cfa
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-03-18 11:22:30 +0330

    base & simple trainer added

---
+++ trainer/base.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+from typing import Any
+from argparse import Namespace, ArgumentParser
+from torch.utils.data import DataLoader
+
+
+class Trainer(ABC):
+    @abstractmethod
+    def create_model(self, args: Namespace):
+        pass
+
+    @abstractmethod
+    def load_last_checkpoint(self, args: Namespace) -> Any:
+        pass
+
+    @abstractmethod
+    def create_dataloader(self) -> DataLoader:
+        pass
+
+    @abstractmethod
+    def pre_train(self):
+        pass
+
+    @abstractmethod
+    def train(self) -> Any:
+        pass
+
+    @abstractmethod
+    def train_step(self, *args: Any, **kwargs: Any) -> Any:
+        pass
+
+    @abstractmethod
+    def save_step(self):
+        pass
+
+    @abstractmethod
+    def post_train(self):
+        pass
+
+    @abstractmethod
+    def get_arg_parser(self) -> ArgumentParser:
+        pass
+
+    @abstractmethod
+    def create_default_args(self) -> Namespace:
+        pass
---
+++ trainer/simple.py
@@ -0,0 +1,62 @@
+import os
+from abc import abstractmethod
+from typing import Any
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.tensorboard.writer import SummaryWriter
+import logging
+from tqdm import tqdm
+from .base import Trainer
+
+
+class SimpleTrainer(Trainer):
+    @abstractmethod
+    def load_last_checkpoint(self) -> tuple[nn.Module, optim.Optimizer, int]:
+        pass
+
+    @abstractmethod
+    def train_step(self, model: nn.Module, batch: Any) -> torch.Tensor:
+        pass
+
+    def __init__(self):
+        super().__init__()
+
+        parser = self.get_arg_parser()
+        self.args = parser.parse_args()
+
+    def train(self):
+        dataloader = self.create_dataloader()
+
+        parser = self.get_arg_parser()
+        args = parser.parse_args()
+
+        model, optimizer, last_epoch = self.load_last_checkpoint()
+
+        self.pre_train()
+
+        logger = SummaryWriter(os.path.join(args.prefix, "runs", args.run_name))
+
+        len_data = len(dataloader)
+
+        for epoch in range(last_epoch, args.epochs):
+            logging.info(f"Starting epoch {epoch+1}")
+
+            for i, batch in enumerate(
+                tqdm(
+                    dataloader,
+                    desc=f"Training [{epoch + 1}/{args.epochs}]",
+                )
+            ):
+                loss = self.train_step(model=model, batch=batch)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                logger.add_scalar("Loss", loss.item(), global_step=epoch * len_data + i)
+
+            if (epoch + 1) % args.save_freq == 0:
+                self.save_step()
+
+        self.post_train()
