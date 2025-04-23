54f779aeb1b7837010334d44b3e12b5c99c5c686
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-04-23 12:30:16 +0330

    DDIMTrainer added

---
+++ trainer/models/ddim.py
@@ -0,0 +1,39 @@
+from torch import nn
+from models.diffusion.base import Diffusion
+from models.diffusion.ddim import DDIM_Params, DDIM
+from trainer.models.ddpm import DDPMTrainer
+
+
+class DDIMTrainer(DDPMTrainer):
+    def create_diffusion_model(self, eps_theta: nn.Module) -> Diffusion:
+        args = self.args
+
+        if args.model_type == "sde":
+            params = DDIM_Params(args.device)
+            params.eps_theta = eps_theta
+            params.sigma_min = args.sigma_min
+            params.sigma_max = args.sigma_max
+            params.beta_start = args.beta_start
+            params.beta_end = args.beta_end
+            params.input_size = (args.in_channels, args.img_size, args.img_size)
+
+            return DDIM(params)
+
+    def create_default_args(self):
+        args = super().create_default_args()
+        args.run_name = "DDIM"
+        args.model_type = "sde"
+        args.sigma_min = 0.02
+        args.sigma_max = 50
+
+        return args
+
+    def get_arg_parser(self):
+        parser = super().get_arg_parser()
+
+        d_args = self.create_default_args()
+
+        parser.add_argument("--sigma_min", type=float, default=d_args.sigma_min)
+        parser.add_argument("--sigma_max", type=float, default=d_args.sigma_max)
+
+        return parser
