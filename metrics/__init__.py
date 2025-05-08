7d49b44c81adde231b466b8a85de01c753c8acb5
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-05-08 13:21:08 +0330

    lpips metric added

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,6 +1,9 @@
 from .fid import FID, FIDMeta
+from .lpips import LPIPS, LPIPSMeta
 
 __all__ = (
     "FID",
     "FIDMeta",
+    "LPIPS",
+    "LPIPSMeta",
 )
---
+++ metrics/lpips.py
@@ -0,0 +1,45 @@
+from typing import Callable, Literal, Optional
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import lpips
+from .base import Metric, MetricMeta
+
+
+class LPIPSMeta(MetricMeta):
+    net_type: Optional[Literal["alex", "vgg"]]
+    forward_method: Callable[[torch.Tensor], torch.Tensor]
+
+    def __init__(self):
+        super().__init__()
+
+        self.net_type = "alex"
+
+
+class LPIPS(Metric):
+    def __init__(self, meta: LPIPSMeta):
+        super().__init__(meta)
+
+        self.meta = meta
+
+        self.model = lpips.LPIPS(net=self.meta.net_type, spatial=False)
+        self.model.eval()
+        self.model.to(self.meta.device)
+
+    @torch.no_grad()
+    def calc(self, dataloader: DataLoader):
+        scores = []
+
+        for batch in tqdm(dataloader, desc="Calculating"):
+            if isinstance(batch, list):
+                batch = batch[0]
+
+            real_images = batch.to(self.meta.device)
+            fake_images = self.meta.forward_method(real_images)
+
+            score = self.model(real_images, fake_images, normalize=False).squeeze()
+            scores.append(score.mean())
+
+        scores = torch.tensor(scores, device=self.meta.device)
+
+        return torch.mean(scores)
