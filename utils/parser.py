62c2ab667a78c94e5dfef11b44fb92181612de72
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-07-29 13:45:59 +0330

    ConfigParser updated

--- utils/parser.py
+++ utils/parser.py
@@ -10,6 +10,7 @@ import yaml
 class ConfigParser:
     config: dict[str, Any]
     local: Optional[ModuleType]
+    variables: dict[str, Any]
 
     def __init__(self, config_path: Union[str, Path]):
         root, _ = os.path.split(config_path)
@@ -22,6 +23,8 @@ class ConfigParser:
             self.local = self._load_python_module(os.path.join(root, local_path))
         else:
             self.local = None
+
+        self.variables = {}
 
     def _load_python_module(self, path: str) -> ModuleType:
         if not os.path.isfile(path):
@@ -48,12 +51,18 @@ class ConfigParser:
             raise ImportError(f"Could not import module '{module_path}': {e}") from e
 
     def _resolve_entry(self, entry: Any) -> Any:
+        if isinstance(entry, str) and entry.startswith("args."):
+            key = entry.split(".")[1]
+            entry = self.variables.get(key)
+
+            return entry
+
         if not isinstance(entry, dict):
             return entry
 
         module_path = entry["module"]
         symbol_name = entry["source"]
-        should_call = entry.get("call", True)
+        call = entry.get("call", True)
 
         obj = None
 
@@ -62,14 +71,28 @@ class ConfigParser:
         else:
             obj = self._load_object(module_path, symbol_name)
 
-        if callable(obj) and should_call:
+        if callable(obj) and call:
             args = entry.get("args", {})
 
             if isinstance(args, dict):
                 for k in args:
                     args[k] = self._resolve_entry(args[k])
+                    self.variables[k] = args[k]
 
-            return obj(**args)
+            if isinstance(call, bool):
+                return obj(**args)
+
+            if not hasattr(obj, call):
+                raise AttributeError(
+                    f"Module '{symbol_name}' has no attribute '{call}'"
+                )
+
+            fn = getattr(obj, call)
+
+            if not callable(fn):
+                raise TypeError(f"{fn} is not callable")
+
+            return fn(**args)
 
         return obj
 
