8b715585f3e0715fa4882c431c5ffc1980bd790b
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-08-22 20:17:19 +0330

    VAE_KL model added

---
+++ models/vae/kl.py
@@ -0,0 +1,121 @@
+from typing import Literal, Optional
+import torch
+from diffusers.models import AutoencoderKL
+from diffusers.models.autoencoders.vae import DiagonalGaussianDistribution
+from .base import VAE
+from .modules import LPIPSWithDiscriminator
+
+
+class VAE_KL_Params:
+    in_channels: int
+    out_channels: int
+    embedding_dim: int
+    pretrained_model_name_or_path: Optional[str]
+    lpips_model_path: Optional[str]
+    disc_start: int
+    log_var_init: float
+    kl_weight: float
+    pixel_loss_weight: float
+    disc_n_layers: int
+    disc_in_channels: int
+    disc_factor: float
+    disc_weight: float
+    perceptual_weight: float
+    disc_conditional: bool
+    disc_loss: Literal["hinge", "vanilla"]
+
+    def __init__(self, device: torch.device):
+        self.device = device
+
+        self.in_channels = 3
+        self.out_channels = 3
+        self.embedding_dim = 4
+        self.pretrained_model_name_or_path = None
+        self.lpips_model_path = None
+        self.log_var_init = 0.0
+        self.kl_weight = 1.0
+        self.pixel_loss_weight = 1.0
+        self.disc_n_layers = 3
+        self.disc_in_channels = 3
+        self.disc_factor = 1.0
+        self.disc_weight = 1.0
+        self.perceptual_weight = 1.0
+        self.disc_conditional = False
+        self.disc_loss = "hinge"
+
+
+class VAE_KL(VAE):
+    def __init__(self, args: VAE_KL_Params):
+        super().__init__()
+
+        self.args = args
+
+        if args.pretrained_model_name_or_path is not None:
+            self.vae = AutoencoderKL.from_pretrained(args.pretrained_model_name_or_path)
+        else:
+            self.vae = AutoencoderKL(
+                in_channels=args.in_channels,
+                out_channels=args.out_channels,
+                latent_channels=args.embedding_dim,
+            )
+
+        self.loss = LPIPSWithDiscriminator(
+            lpips_model_path=args.lpips_model_path,
+            disc_start=args.disc_start,
+            log_var_init=args.log_var_init,
+            kl_weight=args.kl_weight,
+            pixel_loss_weight=args.pixel_loss_weight,
+            disc_n_layers=args.disc_n_layers,
+            disc_in_channels=args.disc_in_channels,
+            disc_factor=args.disc_factor,
+            disc_weight=args.disc_weight,
+            perceptual_weight=args.perceptual_weight,
+            disc_conditional=args.disc_conditional,
+            disc_loss=args.disc_loss,
+        )
+
+    def encode(self, x: torch.Tensor, return_dist: bool = False):
+        enc_dist, _ = self.vae.encode(x, return_dict=False)
+
+        if not isinstance(enc_dist, DiagonalGaussianDistribution):
+            raise TypeError("encoded x is not DiagonalGaussianDistribution")
+
+        if return_dist:
+            return enc_dist
+
+        return enc_dist.sample()
+
+    def decode(self, z: torch.Tensor):
+        dec_tensor, _ = self.vae.decode(z, return_dict=False)
+
+        if not isinstance(dec_tensor, torch.Tensor):
+            raise TypeError("decoded z is not Tensor")
+
+        return dec_tensor
+
+    def sample(self, n: int):
+        raise NotImplementedError("sampling is not implemented for kl.")
+
+    @torch.no_grad()
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        z = self.encode(x)
+        x_hat = self.decode(z)
+
+        return x_hat
+
+    def calc_loss(self, x: torch.Tensor, optimizer_idx: int, global_step: int):
+        last_layer = self.vae.decoder.conv_out.weight
+
+        z_dist = self.encode(x, return_dist=True)
+        x_hat = self.decode()
+
+        loss = self.loss(
+            inputs=x,
+            reconstructions=x_hat,
+            posteriors=z_dist,
+            optimizer_idx=optimizer_idx,
+            global_step=global_step,
+            last_layer=last_layer,
+        )
+
+        return loss
--- models/vae/modules.py
+++ models/vae/modules.py
@@ -1,6 +1,9 @@
+from typing import Literal, Optional
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from lpips import LPIPS
+from diffusers.models.autoencoders.vae import DiagonalGaussianDistribution
 
 
 class VectorQuantizer(nn.Module):
@@ -149,3 +152,303 @@ class ResidualStack(nn.Module):
         x = F.relu(x)
 
         return x
+
+
+class PatchGANDiscriminator(nn.Module):
+    def __init__(
+        self,
+        in_channels: int = 3,
+        n_filters: int = 64,
+        n_layers: int = 3,
+    ):
+        super().__init__()
+
+        kernel_size = 4
+        padding = 1
+
+        sequence = [
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=n_filters,
+                kernel_size=kernel_size,
+                stride=2,
+                padding=padding,
+            ),
+            nn.LeakyReLU(0.2, True),
+        ]
+
+        nf_mul = 1
+        nf_mul_prev = 1
+        for n in range(1, n_layers):
+            nf_mul_prev = nf_mul
+            nf_mul = min(2**n, 8)
+            sequence += [
+                nn.Conv2d(
+                    n_filters * nf_mul_prev,
+                    n_filters * nf_mul,
+                    kernel_size=kernel_size,
+                    stride=2,
+                    padding=padding,
+                    bias=False,
+                ),
+                nn.BatchNorm2d(num_features=n_filters * nf_mul),
+                nn.LeakyReLU(0.2, True),
+            ]
+
+        nf_mul_prev = nf_mul
+        nf_mul = min(2**n_layers, 8)
+
+        sequence += [
+            nn.Conv2d(
+                in_channels=n_filters * nf_mul_prev,
+                out_channels=n_filters * nf_mul,
+                kernel_size=kernel_size,
+                stride=1,
+                padding=padding,
+                bias=False,
+            ),
+            nn.BatchNorm2d(num_features=n_filters * nf_mul),
+            nn.LeakyReLU(0.2, True),
+        ]
+
+        sequence += [
+            nn.Conv2d(
+                in_channels=n_filters * nf_mul,
+                out_channels=1,
+                kernel_size=kernel_size,
+                stride=1,
+                padding=padding,
+            )
+        ]
+
+        self.main = nn.Sequential(*sequence)
+
+    def forward(self, input: torch.Tensor):
+        return self.main(input)
+
+    def init_weights(self):
+        for m in self.modules():
+            classname = m.__class__.__name__
+            if classname.find("Conv") != -1:
+                nn.init.normal_(m.weight.data, 0.0, 0.02)
+            elif classname.find("BatchNorm") != -1:
+                nn.init.normal_(m.weight.data, 1.0, 0.02)
+                nn.init.constant_(m.bias.data, 0)
+
+
+class LPIPSWithDiscriminator(nn.Module):
+    def __init__(
+        self,
+        lpips_model_path: str,
+        disc_start: int,
+        log_var_init: float = 0.0,
+        kl_weight: float = 1.0,
+        pixel_loss_weight: float = 1.0,
+        disc_n_layers: int = 3,
+        disc_in_channels: int = 3,
+        disc_factor: float = 1.0,
+        disc_weight: float = 1.0,
+        perceptual_weight: float = 1.0,
+        disc_conditional: bool = False,
+        disc_loss: Literal["hinge", "vanilla"] = "hinge",
+    ):
+        super().__init__()
+
+        self.kl_weight = kl_weight
+        self.pixel_weight = pixel_loss_weight
+        self.perceptual_loss = LPIPS(model_path=lpips_model_path).eval()
+        self.perceptual_weight = perceptual_weight
+
+        self.log_var = nn.Parameter(torch.ones(size=()) * log_var_init)
+
+        self.discriminator = PatchGANDiscriminator(
+            in_channels=disc_in_channels,
+            n_layers=disc_n_layers,
+        )
+
+        self.discriminator.init_weights()
+
+        self.discriminator_iter_start = disc_start
+        self.disc_loss = disc_loss
+        self.disc_factor = disc_factor
+        self.discriminator_weight = disc_weight
+        self.disc_conditional = disc_conditional
+
+    def _calculate_adaptive_weight(
+        self,
+        nll_loss: torch.Tensor,
+        g_loss: torch.Tensor,
+        last_layer: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        if last_layer is not None:
+            nll_grads = torch.autograd.grad(
+                nll_loss,
+                last_layer,
+                retain_graph=True,
+            )[0]
+            g_grads = torch.autograd.grad(
+                g_loss,
+                last_layer,
+                retain_graph=True,
+            )[0]
+        else:
+            nll_grads = torch.autograd.grad(
+                nll_loss,
+                self.last_layer[0],
+                retain_graph=True,
+            )[0]
+            g_grads = torch.autograd.grad(
+                g_loss,
+                self.last_layer[0],
+                retain_graph=True,
+            )[0]
+
+        d_weight = torch.norm(nll_grads) / (torch.norm(g_grads) + 1e-4)
+        d_weight = torch.clamp(d_weight, 0.0, 1e4).detach()
+        d_weight = d_weight * self.discriminator_weight
+
+        return d_weight
+
+    def _calc_loss(
+        self,
+        logits_real: torch.Tensor,
+        logits_fake: torch.Tensor,
+    ) -> torch.Tensor:
+        if self.disc_loss == "vanilla":
+            d_loss = 0.5 * (
+                torch.mean(torch.nn.functional.softplus(-logits_real))
+                + torch.mean(torch.nn.functional.softplus(logits_fake))
+            )
+
+            return d_loss
+
+        if self.disc_loss == "hinge":
+            loss_real = torch.mean(F.relu(1.0 - logits_real))
+            loss_fake = torch.mean(F.relu(1.0 + logits_fake))
+            d_loss = 0.5 * (loss_real + loss_fake)
+
+            return d_loss
+
+    def _adopt_weight(
+        self,
+        weight: torch.Tensor,
+        global_step: int,
+        threshold: int = 0,
+        value: float = 0.0,
+    ):
+        if global_step < threshold:
+            weight = value
+        return weight
+
+    def forward(
+        self,
+        inputs: torch.Tensor,
+        reconstructions: torch.Tensor,
+        posteriors: DiagonalGaussianDistribution,
+        optimizer_idx: int,
+        global_step: int,
+        last_layer: Optional[torch.Tensor] = None,
+        cond: Optional[torch.Tensor] = None,
+        weights: Optional[torch.Tensor] = None,
+    ):
+        rec_loss = torch.abs(inputs.contiguous() - reconstructions.contiguous())
+
+        if self.perceptual_weight > 0:
+            p_loss = self.perceptual_loss(
+                inputs.contiguous(),
+                reconstructions.contiguous(),
+            )
+
+            rec_loss = rec_loss + self.perceptual_weight * p_loss
+
+        nll_loss = rec_loss / torch.exp(self.log_var) + self.log_var
+
+        weighted_nll_loss = nll_loss
+        if weights is not None:
+            weighted_nll_loss = weights * nll_loss
+
+        weighted_nll_loss = torch.sum(weighted_nll_loss) / weighted_nll_loss.shape[0]
+        nll_loss = torch.sum(nll_loss) / nll_loss.shape[0]
+        kl_loss = posteriors.kl()
+        kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
+
+        if optimizer_idx == 0:
+            if cond is None:
+                logits_fake = self.discriminator(reconstructions.contiguous())
+            else:
+                logits_fake = self.discriminator(
+                    torch.cat(
+                        (
+                            reconstructions.contiguous(),
+                            cond,
+                        ),
+                        dim=1,
+                    )
+                )
+            g_loss = -torch.mean(logits_fake)
+
+            if self.disc_factor > 0.0:
+                try:
+                    d_weight = self._calculate_adaptive_weight(
+                        nll_loss,
+                        g_loss,
+                        last_layer=last_layer,
+                    )
+                except RuntimeError:
+                    if self.training:
+                        raise ValueError("training is True")
+
+                    d_weight = torch.tensor(0.0)
+            else:
+                d_weight = torch.tensor(0.0)
+
+            disc_factor = self._adopt_weight(
+                self.disc_factor,
+                global_step,
+                threshold=self.discriminator_iter_start,
+            )
+
+            loss = (
+                weighted_nll_loss
+                + self.kl_weight * kl_loss
+                + d_weight * disc_factor * g_loss
+            )
+
+            return loss
+
+        if optimizer_idx == 1:
+            if cond is None:
+                logits_real = self.discriminator(inputs.contiguous().detach())
+                logits_fake = self.discriminator(reconstructions.contiguous().detach())
+            else:
+                logits_real = self.discriminator(
+                    torch.cat(
+                        (
+                            inputs.contiguous().detach(),
+                            cond,
+                        ),
+                        dim=1,
+                    )
+                )
+                logits_fake = self.discriminator(
+                    torch.cat(
+                        (
+                            reconstructions.contiguous().detach(),
+                            cond,
+                        ),
+                        dim=1,
+                    )
+                )
+
+            disc_factor = self._adopt_weight(
+                self.disc_factor,
+                global_step,
+                threshold=self.discriminator_iter_start,
+            )
+
+            d_loss = disc_factor * self._calc_loss(
+                logits_real=logits_real,
+                logits_fake=logits_fake,
+            )
+
+            return d_loss
