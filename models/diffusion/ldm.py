b17719bf2347c9da9b92b471810ea315f0a0e1b3
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-08-28 21:24:26 +0330

    ldm refactored

--- models/diffusion/ldm.py
+++ models/diffusion/ldm.py
@@ -1,7 +1,6 @@
 from typing import Optional
 import torch
 import torch.nn as nn
-from utils import fill_tail_dims
 from utils.args import with_kwargs, KWargs
 from .ddpm import DDPM, DDPM_Params, DDPM_Forward, DDPM_Reverse
 
@@ -41,22 +40,6 @@ class LDM_Reverse(DDPM_Reverse):
         self.forward_sde = forward_sde
         self.args = args
 
-    @torch.no_grad()
-    def forward(
-        self,
-        x_t: torch.Tensor,
-        y: torch.Tensor,
-        dt: float = 1e-2,
-        use_sde: bool = True,
-    ) -> torch.Tensor:
-        KWargs().insert(self.forward_sde.s_theta, y=y)
-
-        x_0 = super().forward(x_t, dt, use_sde)
-
-        KWargs().drop(self.forward_sde.s_theta)
-
-        return x_0
-
 
 class LDM(DDPM):
     def __init__(self, args: LDM_Params):
@@ -67,13 +50,33 @@ class LDM(DDPM):
         self.f_sde = LDM_Forward(args)
         self.r_sde = LDM_Reverse(self.f_sde, args)
 
+    def predict_noise(
+        self,
+        x_t: torch.Tensor,
+        y: torch.Tensor,
+        t: torch.Tensor,
+    ):
+        KWargs().insert(self.f_sde.s_theta, y=y)
+
+        noise = super().predict_noise(x_t, t)
+
+        KWargs().drop(self.f_sde.s_theta)
+
+        return noise
+
     def reverse(
         self,
         x_t: torch.Tensor,
         y: torch.Tensor,
         use_sde: bool = True,
     ):
-        return self.r_sde(x_t, y, use_sde=use_sde)[-1]
+        KWargs().insert(self.f_sde.s_theta, y=y)
+
+        x_0 = super().reverse(x_t, use_sde)
+
+        KWargs().drop(self.f_sde.s_theta)
+
+        return x_0
 
     def sample(
         self,
@@ -81,9 +84,13 @@ class LDM(DDPM):
         y: torch.Tensor,
         use_sde: bool = True,
     ):
-        x_t = torch.randn(size=(n, *self.args.input_size), device=self.args.device)
+        KWargs().insert(self.f_sde.s_theta, y=y)
 
-        return self.r_sde(x_t, y, use_sde=use_sde)[-1]
+        x_0 = super().sample(n, use_sde)
+
+        KWargs().drop(self.f_sde.s_theta)
+
+        return x_0
 
     def calc_loss(
         self,
@@ -91,15 +98,13 @@ class LDM(DDPM):
         t: torch.Tensor,
         y: torch.Tensor,
     ):
-        x_t = self.f_sde.analytical_sample(x_0, t)
-        lambda_t = self.f_sde.analytical_var(t)
+        KWargs().insert(self.f_sde.s_theta, y=y)
 
-        score_pred = self.f_sde.s_theta(t, x_t, y)
-        score_true = self.f_sde.analytical_score(x_t, x_0, t)
+        loss = super().calc_loss(x_0, t)
 
-        loss = fill_tail_dims(lambda_t, x_t) * ((score_pred - score_true) ** 2)
+        KWargs().drop(self.f_sde.s_theta)
 
-        return loss.mean()
+        return loss
 
     def train(self, with_tau: bool = True):
         super().train()
