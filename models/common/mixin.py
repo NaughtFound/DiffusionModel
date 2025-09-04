2fe41f9eea1c295ca2010531d5703b02acdc7765
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-09-04 15:09:02 +0330

    ModelMixin updated

--- models/common/mixin.py
+++ models/common/mixin.py
@@ -1,8 +1,20 @@
-from typing import Self
+from typing import Any, Self
 import argparse
+from copy import deepcopy
 
 
 class ModelMixin:
     @classmethod
+    def _from_kwargs(cls, kwargs: dict[str, Any]) -> Self:
+        return cls(**kwargs)
+
+    @classmethod
     def from_params(cls, params: argparse.Namespace) -> Self:
-        return cls(**params.__dict__)
+        return cls._from_kwargs(params.__dict__)
+
+    @classmethod
+    def from_params_with_kwargs(cls, params: argparse.Namespace, **kwargs) -> Self:
+        final_kwargs = deepcopy(params.__dict__)
+        final_kwargs.update(kwargs)
+
+        return cls._from_kwargs(final_kwargs)
