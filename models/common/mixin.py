f4df80bf93182edc2750ae2d19a628d76e6fec5e
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-09-04 21:17:40 +0330

    minor bug fixed in ModelMixin

--- models/common/mixin.py
+++ models/common/mixin.py
@@ -1,3 +1,4 @@
+import inspect
 from typing import Any, Self
 import argparse
 from copy import deepcopy
@@ -6,7 +7,13 @@ from copy import deepcopy
 class ModelMixin:
     @classmethod
     def _from_kwargs(cls, kwargs: dict[str, Any]) -> Self:
-        return cls(**kwargs)
+        sig = inspect.signature(cls.__init__)
+
+        valid_kwargs = {
+            key: value for key, value in kwargs.items() if key in sig.parameters
+        }
+
+        return cls(**valid_kwargs)
 
     @classmethod
     def from_params(cls, params: argparse.Namespace) -> Self:
