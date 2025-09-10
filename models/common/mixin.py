262ef9c74e03b256cdb99c392dfb4a9fc4e74660
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-09-10 16:11:13 +0330

    Self as a return type removed

--- models/common/mixin.py
+++ models/common/mixin.py
@@ -1,12 +1,12 @@
 import inspect
-from typing import Any, Self
+from typing import Any
 import argparse
 from copy import deepcopy
 
 
 class ModelMixin:
     @classmethod
-    def _from_kwargs(cls, kwargs: dict[str, Any]) -> Self:
+    def _from_kwargs(cls, kwargs: dict[str, Any]):
         sig = inspect.signature(cls.__init__)
 
         valid_kwargs = {
@@ -16,11 +16,11 @@ class ModelMixin:
         return cls(**valid_kwargs)
 
     @classmethod
-    def from_params(cls, params: argparse.Namespace) -> Self:
+    def from_params(cls, params: argparse.Namespace):
         return cls._from_kwargs(params.__dict__)
 
     @classmethod
-    def from_params_with_kwargs(cls, params: argparse.Namespace, **kwargs) -> Self:
+    def from_params_with_kwargs(cls, params: argparse.Namespace, **kwargs):
         final_kwargs = deepcopy(params.__dict__)
         final_kwargs.update(kwargs)
 
